5b231cca381416cb3b1bd146413d143a9caf1ea4
chunrigongjie <274669692@qq.com>
2017-03-25 15:54:49 +0800

    add

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,24 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by fan on 2017/3/24.
+ */
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+
+        @SerializedName("loc")
+        public  String updateTime;
+    }
+}
